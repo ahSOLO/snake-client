c7b66bd6ea1c6e94120a2229407107986bd3e0d2
Andrew Han <junmenghan@gmail.com>
2021-01-13 22:42:47 +0000

    added controls

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
+const { stdin } = require("process");
+
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.on('data', handleUserInput);
